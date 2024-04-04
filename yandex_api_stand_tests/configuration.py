284d58d9a894cdb7833682c6457640e4b404bbd2
NigelDeluxe <i.suvorov.2110@gmail.com>
2024-04-04 08:53:46 +0200

    Autotests for the firstName parameter when creating a user in Yandex.Stand using the Yandex.Stand API

---
+++ yandex_api_stand_tests/configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c5827834-cfad-44de-95a3-24e27a0a6dc7.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ yandex_api_stand_tests/sender_stand_request.py
@@ -0,0 +1,12 @@
+import requests
+import configuration
+import data
+
+def post_new_user(user_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=user_body,
+                         headers=data.headers)
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH,
+                        headers=data.headers)
